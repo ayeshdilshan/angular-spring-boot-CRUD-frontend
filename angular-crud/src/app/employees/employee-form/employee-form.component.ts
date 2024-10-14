ded61be4615d5e28cca2d696a5016d12fd11ed58
ayesh <ayeshjayasundara0725@gmail.com>
2024-10-15 00:31:19 +0530

    add new employee form created

--- angular-crud/src/app/employees/employee-form/employee-form.component.ts
+++ angular-crud/src/app/employees/employee-form/employee-form.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogRef } from '@angular/material/dialog';
+import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-form',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule],
   templateUrl: './employee-form.component.html',
   styleUrl: './employee-form.component.css'
 })
 export class EmployeeFormComponent {
+  employeeForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private dialogRef: MatDialogRef<EmployeeFormComponent>) {
+    this.employeeForm = this.fb.group({
+      name: ['', Validators.required],
+      position: ['', Validators.required],
+      department: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  onSave() {
+    if (this.employeeForm.valid) {
+      // Logic to save employee
+      this.dialogRef.close(this.employeeForm.value);
+    }
+  }
+
+  onCancel() {
+    this.dialogRef.close();
+  }
 }
--- angular-crud/src/app/header/header.component.ts
+++ angular-crud/src/app/header/header.component.ts
@@ -3,6 +3,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MAT_FORM_FIELD } from '@angular/material/form-field';
 import { RouterLink } from '@angular/router';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatDialog } from '@angular/material/dialog';
+import { EmployeeFormComponent } from '../employees/employee-form/employee-form.component';
 
 @Component({
   selector: 'app-header',
@@ -12,5 +14,12 @@ import { MatMenuModule } from '@angular/material/menu';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  constructor(private dialog: MatDialog) {}
 
+  openSaveDialog() {
+    const dialogRef = this.dialog.open(EmployeeFormComponent, {
+      width: '400px',
+    });
+
+}
 }
